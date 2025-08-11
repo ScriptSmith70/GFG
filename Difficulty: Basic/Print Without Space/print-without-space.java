77d41b0825299db36cbde43bb5c7143d75c79bda
ScriptSmith70 <insidestory100@gmail.com>
2025-08-11 14:22:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Without Space/print-without-space.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Solution {
+    public void utility(Scanner scn) {
+        
+        String s1 = scn.nextLine();  
+        String s2 = scn.nextLine();  
+
+        
+        System.out.println(s1 + s2); 
+    }
+}
