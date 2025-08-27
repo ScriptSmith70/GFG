a397d5823f568e2d6f20768faf8613daf6361efc
ScriptSmith70 <insidestory100@gmail.com>
2025-08-27 09:11:51 +0530

    Update reverse-a-string.java

--- Java Programs/Reverse a String/reverse-a-string.java
+++ Java Programs/Reverse a String/reverse-a-string.java
@@ -1,11 +1,10 @@
 class Solution {
     public static String reverseString(String s) {
-        // code here
          int n= s.length();
         String k="";
         for(int i=n-1;i>=0;i--){
             k=k+s.charAt(i);
         }
-        return k;
+        return k;  
     }
 }
