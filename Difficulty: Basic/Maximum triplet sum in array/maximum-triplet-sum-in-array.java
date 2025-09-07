83d7bf335e845576e72960e8b5b5ddd43ab995e6
ScriptSmith70 <insidestory100@gmail.com>
2025-09-07 17:54:28 +0530

    Update maximum-triplet-sum-in-array.java

--- Difficulty: Basic/Maximum triplet sum in array/maximum-triplet-sum-in-array.java
+++ Difficulty: Basic/Maximum triplet sum in array/maximum-triplet-sum-in-array.java
@@ -2,7 +2,7 @@ class Solution {
     // Function to find the maximum triplet sum in the array.
     public int maxTripletSum(List<Integer> arr) {
         // Complete the function
-        Collections.sort(arr);
+        Collections.sort(arr); 
         int n= arr.size();
         int sum=0;
         for(int i=n-1;i>=n-3;i--){
